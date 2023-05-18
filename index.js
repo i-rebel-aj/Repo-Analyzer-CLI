ae5cf5c1b1ad991fb035c918d11a78d6070ad144
Akshay Jain <akshay@gocobalt.io>
2023-05-19 03:08:00 +0530

    feat(stats): Showing total stats of contributions, lines_added, lines_deleted

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
 const config=require('./config.json')
-const stats=[]
+const util=require('util')
+const stats={
+    summary: {},
+    user_wise_stats:{}
+}
 async function getPathString(){
     console.log(config.local_repos.join(' '))
 }
 async function parseOutFileData(){
     const parsed_output=require('./output.json')
     //TODO: Add Collective Graph here
-    console.log('Output is', parsed_output)
+    // console.log('Output is', util.inspect(parsed_output[0].stats['Akshay Jain'], null) )
+    for (const repo_stat of parsed_output) {
+        for (const name in repo_stat.stats) {
+            if(!stats.summary[name]){
+                stats.summary[name]={
+                    total_contributions: 0,
+                    total_lines_added: 0,
+                    total_lines_deleted: 0
+                }
+            }
+            if (Object.hasOwnProperty.call(repo_stat.stats, name)) {
+                if(!stats.user_wise_stats[name]?.length){
+                    stats.user_wise_stats[name]=[{
+                        repo_path: repo_stat.path,
+                        contributions: repo_stat.stats[name].contributions,
+                        lines_added: repo_stat.stats[name].lines_added,
+                        lines_deleted: repo_stat.stats[name].lines_deleted
+                    }]
+                }else{
+                    stats.user_wise_stats[name].push({
+                        repo_path: repo_stat.path,
+                        contributions: repo_stat.stats[name].contributions,
+                        lines_added: repo_stat.stats[name].lines_added,
+                        lines_deleted: repo_stat.stats[name].lines_deleted
+                    })
+                }
+                stats.summary[name]['total_contributions']+=repo_stat.stats[name].contributions
+                stats.summary[name]['total_lines_added']+=repo_stat.stats[name].lines_added
+                stats.summary[name]['total_lines_deleted']+=repo_stat.stats[name].lines_deleted           
+            }
+        }
+    }
+    console.log('User stats are', stats )
+
 }
 
 
--- repo-stat.sh
+++ repo-stat.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-out_file='./out_file'
 json_output="["
 for arg in "$@"; do
   repo_json="{\"path\":\"$arg\",\"stats\":"
   output=$(git -C "$arg" shortlog -sn)
   repo_stat_json="{"
   while IFS=$'\t' read -r commits author; do
-    repo_stat_json+="\"$author\":$commits,"
+    # Get the number of lines added and deleted by the author
+    lines_added=$(git -C "$arg" log --author="$author" --oneline --shortstat | awk '/files? changed/ {s+=$4} /insertions?/ {s+=$4} END {print s}')
+    lines_deleted=$(git -C "$arg" log --author="$author" --oneline --shortstat | awk '/files? changed/ {s+=$6} /deletions?/ {s+=$6} END {print s}')
+    repo_stat_json+="\"$author\":{\"contributions\":$commits,\"lines_added\":${lines_added:-0},\"lines_deleted\":${lines_deleted:-0} },"
   done <<< "$output"
   repo_stat_json="${repo_stat_json%,}" # Remove trailing comma
   repo_stat_json+="}"
